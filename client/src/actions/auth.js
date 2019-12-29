6db6b1dabe362ad95008a8cfee8ebf72e428a3cc
TomPlucinski <tapluci1@gmail.com>
2019-12-28 19:09:02 -0600

    logout feature

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,6 +6,7 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGOUT,
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -89,4 +90,9 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout / Clear profile
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };
